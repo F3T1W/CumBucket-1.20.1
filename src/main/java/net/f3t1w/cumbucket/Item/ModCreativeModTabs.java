d3e0ed116331fc72a61773f96c221051f38e94a7
Vlad Matyushin <particular0010abyss@gmail.com>
2024-02-02 04:58:14 +0500

    Added CustomItems and CreativeModTab

---
+++ src/main/java/net/f3t1w/cumbucket/Item/ModCreativeModTabs.java
@@ -0,0 +1,31 @@
+package net.f3t1w.cumbucket.Item;
+
+import net.f3t1w.cumbucket.CumBucket;
+import net.f3t1w.cumbucket.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CumBucket.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> CUMBUCKET = CREATIVE_MODE_TABS.register("cumbucket_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.KISKA.get()))
+                    .title(Component.translatable("creativetab.cumbucket_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.KISKA.get());
+                        output.accept(ModItems.CUM_COOKIE.get());
+
+                        output.accept(ModBlocks.F_BLOCK.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
